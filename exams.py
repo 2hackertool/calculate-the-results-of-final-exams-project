607988a6901f93ad287e59414f9ff24323f679de
2hackertool <160114976+2hackertool@users.noreply.github.com>
2024-03-08 15:22:16 +0300

    Create exams.py

---
+++ exams.py
@@ -0,0 +1,24 @@
+from pysytle import Write, Colors
+import os
+import time
+import pyphisher
+time.sleep(2)
+print('''___________________________________''')
+print()
+os.system("figlet exams | lolcat")
+print()
+print('''___________________________________''')
+print()
+Write.Print(f"coded by hackertool", Colors.red_to_blue, interval=0.05)
+print()
+Write.Print(f"this program for calculating grade student", Colors.red_to_blue, interval=0.05)
+print()
+math = input("type your math grade : ")
+E = input("type your English grade : ")
+A = input("type your arabic grade : ")
+h = input("type your history grade : ")
+i = input("type your islamy grade : ")
+s = input("type your science grade : ")
+your_final = "the result of your final exams is"
+logic = float(math) + float(E) + float(A) + float(h) + float(i) + float(s) / 100
+print(Colors.purple + your_final +" "+ str(logic))
